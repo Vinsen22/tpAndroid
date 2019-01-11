c7460f0b2211a7d4a4735c83922525fc75586739
Vinsen <Vinsen.Thamboo@gmail.com>
2019-01-11 12:42:34 +0100

    transition d'une activité à une autre

---
+++ app/src/main/java/com/example/vinsen/tpandroid/DataActivity.kt
@@ -0,0 +1,13 @@
+package com.example.vinsen.tpandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class DataActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_data)
+
+    }
+}
